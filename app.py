6bbfcbfa2d4289617ce035da36ccb15afd97e979
seeun98 <saeeun5659@gmail.com>
2020-11-11 21:46:32 +0900

    init

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from datetime import datetime
-
-
+import uuid
 
 client = MongoClient('localhost', 27017)
 db = client.mindConnecting
@@ -72,22 +71,22 @@ def indexPost():
         # for i in scheduleList:
         #     if name == scheduleList[i]['subject']:
         #         print(name)
-        #         #여기의 class_id를 가져온다.
-        #         #class_id에 맞는 professorCommunicate 페이지로 보내준다.
+        #         #여기의 code를 가져온다.
+        #         #code에 맞는 professorCommunicate 페이지로 보내준다.
     return render_template("index.html")
 
 # #professorCommunicate_list
-# #시간표 누를 시 -> 그 class_id에 해당하는 메인페이지인(교수님 공지 게시판)이 나온다
-# @app.route("/professorCommunicatelist/<class_id>", methods=['GET'])
-# def professorCommunicatelist_get(class_id):
-#     professorCommunicate_list = list(db.professorQuestion.find({'class_id': class_id},{'_id': 0}))
+# #시간표 누를 시 -> 그 code에 해당하는 메인페이지인(교수님 공지 게시판)이 나온다
+# @app.route("/professorCommunicatelist/<code>", methods=['GET'])
+# def professorCommunicatelist_get(code):
+#     professorCommunicate_list = list(db.professorQuestion.find({'code': code},{'_id': 0}))
 #     print(professorCommunicate_list)
 #     return jsonify({'result':'success', 'items': professorCommunicate_list})
 #
 #
-# @app.route("/professorCommunicate/<class_id>", methods=['GET'])
-# def professorCommunicate(class_id):
-#     return render_template("professorCommunicatelist.html", class_id=class_id)
+# @app.route("/professorCommunicate/<code>", methods=['GET'])
+# def professorCommunicate(code):
+#     return render_template("professorCommunicatelist.html", code=code)
 #
 # @app.route("/QuestionFrom", methods=['GET'])
 # def QuestionForm():
@@ -99,27 +98,28 @@ def indexPost():
 #-----------------------------------------------------------------------------------------------------------------
 #게시판 창구
 
-@app.route("/communicate/<class_id>", methods=['GET'])
-def communicate_page(class_id):
-    return render_template('communicate.html', class_id=class_id)
+@app.route("/communicate/<code>", methods=['GET'])
+def communicate_page(code):
+    return render_template('communicate.html', code=code)
 
-@app.route("/communicate/<class_id>/list", methods=['GET'])
-def communicate_list_get(class_id):
-    communicate_list = list(db.communicate.find({'class_id': class_id}, {'_id': 0}))
+@app.route("/communicate/<code>/list", methods=['GET'])
+def communicate_list_get(code):
+    communicate_list = list(db.communicate.find({'code': code}, {'_id': 0}))
 
     return jsonify({'result': 'success', 'items': communicate_list})
 
 
-
 @app.route("/communicate", methods=['POST'])
 def communicate_post():
-    class_id = request.form['class_id']
+    id = uuid.uuid4().hex
+    code = request.form['code']
     title = request.form['title']
     content = request.form['content']
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     communicateInfo = {
-        'class_id': class_id,
+        'id': id,
+        'code': code,
         'title' : title,
         'content' : content,
         'timestamp': timestamp
@@ -127,6 +127,53 @@ def communicate_post():
 
     db.communicate.insert_one(communicateInfo)
     return jsonify({'result': 'success'})
+
+
+@app.route("/review_page/<code>/<communicate_id>", methods=['GET'])
+def review_page(code, communicate_id):
+    return render_template("communicateRE.html", code = code, communicate_id = communicate_id)
+
+# 댓글 구성요소
+# id, communicate_id, content
+#댓글 불러오기
+@app.route("/review_page/<code>/<communicate_id>/comments", methods=['GET'])
+def review_page_comments_list(communicate_id, comment_id, content):
+    comment_list = list(db.comment.find({'communicate_id' : communicate_id, 'comment_id' : comment_id, 'content' : content}))
+    return jsonify({'result': 'success', 'items': comments_list })
+
+#댓글 등록 누를때 댓글 데이터베이스에 저장하기
+@app.route("/comment", methods=['POST'])
+def comment_page_post():
+    comment_id = uuid.uuid4().hex
+    code = request.form['code']
+    communicate_id = request.form['communicate_id']
+    content = request.form['content']
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    communicateCommentInfo ={
+        'comment_id' : comment_id,
+        'communicate_id' : communicate_id,
+        'code' : code,
+        'content' : content,
+        'timestamp' : timestamp
+    }
+
+    db.communicateComment.insert_one(communicateCommentInfo)
+    return jsonify({'result': 'success'})
+
+#댓글 등록 누를때 댓글 데이터베이스에 저장하기
+@app.route("/comment/<communicate_id>", methods=['GET'])
+def comment_page_get(communicate_id):
+    found_comments = list(db.communicateComment.find({'communicate_id': communicate_id}, {'_id': False}))
+    return jsonify({'result': 'success', 'items': found_comments})
+
+#------댓글 페이지--------------
+#게시글 하나 눌렀을때 -> 그 게시글에 대한 댓글 페이지로 넘어감
+#1. 그 게시글에 대한 페이지 보여주기
+#2. 댓글등록 누르면 디비에 저장 후 페이지 새로고침
+#@app.route("/communicate/<code>/REcomments", methods="POST")
+#def communicateREcomments():
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, debug=True)
